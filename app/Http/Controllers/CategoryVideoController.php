cbb3d52bb94c8d3425938d383c4463b6982e49b2
hadi <hadii.mohammadii1379@gmail.com>
2023-12-30 02:27:35 -0800

    update header

--- app/Http/Controllers/CategoryVideoController.php
+++ app/Http/Controllers/CategoryVideoController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class CategoryVideoController extends Controller
 {
     public function index(category $category){
-        $videos = $category->videos()->paginate() ;
+        $videos = $category->videos()->paginate(3) ;
         $title = $category->category ;
         // dd($videos) ;
         return view('videos.index' , compact('videos' , 'title')) ;
